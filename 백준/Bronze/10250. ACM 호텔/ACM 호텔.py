dfa3ca92ea45ffe43934edad132d73baea904d69
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-01-27 16:58:24 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for _ in range(t):
+    h, w, n = map(int, input().split())
+    
+
+    floor = n % h
+   
+    room = (n // h) + 1
+    
+   
+    if floor == 0:
+        floor = h      
+        room -= 1     
+    
+    print(f"{floor}{room:02d}")
