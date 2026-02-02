7774cd8ac029c2230ad3f86f8ee043a27db6ecfd
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-02-02 10:45:11 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,19 @@
+l = int(input())
+s = input()
+
+
+r = 31
+m = 1234567891
+
+ascii_list = []
+temp_sum = 0
+
+for elem in s:
+    ascii_list.append(ord(elem)-96)
+    
+for i in range(len(ascii_list)):
+    cur_sum = ascii_list[i] * (r**i)
+    temp_sum += cur_sum
+
+
+print(temp_sum)
