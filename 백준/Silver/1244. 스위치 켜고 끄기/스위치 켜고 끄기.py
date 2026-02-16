7bf2b4d9586818b3fc8b127eb6fdcee073eea27e
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-02-16 17:20:50 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,50 +1,38 @@
-# 인풋 받기
-switches = int(input())
-lights = list(map(int, input().split()))
-num_students = int(input())
-students = [list(map(int, input().split())) for _ in range(num_students)]
+def change(num):
+    if switch[num] == 0:
+        switch[num] = 1
+    else:
+        switch[num] = 0
+    return
 
-for student in students:
-    # 남자면
-    # 받은 숫자의 배수인 스위치 바꾸기
-    if student[0] == 1:
-        for i in range(1, switches+1):
-            if i%student[1] == 0:
-                if lights[i-1] == 0:
-                    lights[i-1] = 1
-                else:
-                    lights[i-1] = 0
-    # 여자면
-    # 받은 숫자 기준으로 좌우가 대칭이면서 가장 많은 스위치를 포함하는 구간을 찾아서
-    # 다 on/off를 바꿈
-    if student[0] == 2:
-        if lights[student[1] - 1] == 1:
-            lights[student[1] - 1] = 0
-        else:
-            lights[student[1] - 1] = 1
-        n = 1
-        while student[1] - 1 - n >= 0 and student[1] - 1 + n < switches and lights[student[1] - 1 - n] == lights[student[1] - 1 + n]:
-            # 더이상 symmetric아니면 와일문 나가기
+N = int(input())
+switch = [-1] + list(map(int, input().split()))
+students = int(input())
 
-            # 인덱스 (student[1] - 1) 기준
-            # 왼쪽 옆 n개가 오른쪽 옆 n개와 같은지 확인
-            # 같으면 온/오프 바꾸기
-                # 1이면 영으로 바꿈
-                # 0이면 1로 바꿈
+for _ in range(students):
+    gender, num = map(int, input().split())
 
-            if lights[student[1] - 1 - n] == 1:
-                lights[student[1] - 1 - n] = 0
-            elif lights[student[1] - 1 - n] == 0:
-                lights[student[1] - 1 - n] = 1
+    if gender == 1:
+        for i in range(num, N+1, num):
+            change(i)
+    
+    else:
+        change(num)
 
-            if lights[student[1] - 1 + n] == 1:
-                lights[student[1] - 1 + n] = 0
-            elif lights[student[1] - 1 + n] == 0:
-                lights[student[1] - 1 + n] = 1
-            n += 1
+        for i in range(N//2):
+            if num + i > N or num - i < 1:
+                break
 
-# 한줄에 스위치 20개씩 출력
-for j in range(len(lights)):
-    print(lights[j], end = " ") # 옆으로 출력
-    if (j+1)%20 == 0: # 20배수인 곳에서 줄바꿈
+            if switch[num + i] == switch[num - i]:
+                change(num+i)
+                change(num-i)
+            
+            else:
+                break
+
+for i in range(1, N+1):
+    
+    print(switch[i], end = ' ')
+
+    if i % 20 == 0:
         print()
