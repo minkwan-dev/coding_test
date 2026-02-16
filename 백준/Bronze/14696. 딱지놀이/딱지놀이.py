0deb8a4fe8575d28acfe68096e12b7a1d355b942
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-02-16 20:18:35 +0900

    [Bronze I] Title: 딱지놀이, Time: 156 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.py
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -1,28 +1,17 @@
 N = int(input())
 
 for _ in range(N):
+    A = list(map(int, input().split()))[1:]
+    B = list(map(int, input().split()))[1:]
+
     winner = 'D'
-    a_card = list(map(int, input().split()))
-    b_card = list(map(int, input().split()))
 
-    if a_card.count(4) > b_card.count(4):
-        winner = 'A'
-    elif a_card.count(4) < b_card.count(4):
-        winner = 'B'
-    
-    if a_card.count(4) == b_card.count(4) and a_card.count(3) > b_card.count(3):
-        winner = 'A'
-    elif a_card.count(4) == b_card.count(4) and a_card.count(3) < b_card.count(3):
-        winner = 'B'
-
-    if a_card.count(4) == b_card.count(4) and a_card.count(3) == b_card.count(3) and a_card.count(2) > b_card.count(2):
-        winner = 'A'
-    elif a_card.count(4) == b_card.count(4) and a_card.count(3) == b_card.count(3) and a_card.count(2) < b_card.count(2):
-        winner = 'B'
-
-    if a_card.count(4) == b_card.count(4) and a_card.count(3) == b_card.count(3) and a_card.count(2) == b_card.count(2) and a_card.count(1) > b_card.count(1):
-        winner = 'A'
-    elif a_card.count(4) == b_card.count(4) and a_card.count(3) == b_card.count(3) and a_card.count(2) == b_card.count(2) and a_card.count(1) < b_card.count(1):
-        winner = 'B'
+    for shape in range(4, 0, -1):  
+        if A.count(shape) > B.count(shape):
+            winner = 'A'
+            break
+        elif A.count(shape) < B.count(shape):
+            winner = 'B'
+            break
 
     print(winner)
