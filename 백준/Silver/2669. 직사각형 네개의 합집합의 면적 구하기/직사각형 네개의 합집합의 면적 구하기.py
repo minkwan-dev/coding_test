b7842e25ce51fe04c794f9db628d5f3aa80aa859
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-02-16 15:42:27 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
@@ -1,20 +1,15 @@
-board = [[0]*100 for _ in range(100)]
+matrix = [[0 for _ in range(101)] for _ in range(101)]
 
-# 인풋을 받으면서
-# 첫번째 포인트에서 마지막 포인트까지 돌면서
-# 하나씩 해당하는 코디넷에 보드를 1로 채움
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
 
-# 만약 이미 그 코디넷이 1로 채워있다면 넘어가기
-count = 0
-for i in range(4):
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            matrix[i][j] = 1
 
-    rec = list(map(int,input().split()))
-    for y in range(rec[1], rec[3]):
-        for x in range(rec[0], rec[2]):
-            if board[y][x] == 1:
-                pass
-            else:
-                board[y][x] = 1
-                count+=1
+result = 0
 
-print(count)
+for row in matrix:
+    result += sum(row)
+
+print(result)
