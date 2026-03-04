ccc94cc84b69b2affa9c01babdd0bbcf47fa0d2f
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-03-04 15:45:38 +0900

    [Silver IV] Title: 제로, Time: 2484 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,14 +1,14 @@
 K = int(input())
 
-data_list = []
+stack = []
+for k in range(K):
+    num = int(input())
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
 
-for tc in range(1, K + 1):
-    data = int(input())
-    
-    if data == 0:
-        data_list.pop()
-    else: 
-        data_list.append(data)
-        
-
-print(sum(data_list))
+if stack:
+    print(sum(stack))
+else:
+    print(0)
