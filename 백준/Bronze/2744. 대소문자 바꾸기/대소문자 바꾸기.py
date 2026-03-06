c87d916c34853ca11f90d3749b3db952b24db6f8
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-03-06 16:28:14 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,2 +1,10 @@
-word = input()
-print(word.swapcase())
+word =list(input())
+new_word = []
+
+for elem in word:
+	if elem.islower():
+		new_word.append(elem.upper())
+	elif elem.isupper():
+		new_word.append(elem.lower())
+
+print(''.join(new_word))
