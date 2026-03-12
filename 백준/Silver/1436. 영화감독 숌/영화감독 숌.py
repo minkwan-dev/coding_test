dfa4dae8f4d5909fea4ac0d725adf9e10e4bbcb2
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-03-12 12:15:44 +0900

    [Silver V] Title: 영화감독 숌, Time: 752 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,15 +1,15 @@
-n = int(input())
+N = int(input())
 
 cnt = 0
 num = 666
 
 while True:
-    str_data = str(num)
+    str_num = str(num)
 
-    if '666' in str_data:
+    if '666' in str_num:
         cnt += 1
 
-    if n == cnt:
+    if N == cnt:
         break
 
     num += 1
