dff0e1a62ea8c187ac43a16b49883353f0042112
원민관 <117360950+minkwan-dev@users.noreply.github.com>
2026-03-16 11:54:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.98 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,18 @@
+def solution(participant, completion):
+    hash_map = {}
+    
+    # 1. 모든 참가자를 딕셔너리에 추가하고, 등장 횟수를 기록
+    for name in participant:
+        if name not in hash_map:
+            hash_map[name] = 1
+        else:
+            hash_map[name] += 1
+    
+    # 2. 완주자 명단을 순회하며, 해당 이름의 횟수를 차감        
+    for name in completion:
+        hash_map[name] -= 1
+        
+    # 횟수가 0이 아닌 선수가, 완주하지 못한 선수    
+    for name in hash_map:
+        if hash_map[name] > 0:
+            return name
